47197c04caeb4a8bfd1ed85c9eac0e17dd54f11f
bob <bob@>
2023-09-25 01:04:42 +0330

    Fix Build with Latest Code

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,30 +1,10 @@
 import { ErrorHandler, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { CdkMenuModule } from '@angular/cdk/menu';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-
 import { AppComponent } from './app.component';
-import { InputFieldModule } from 'projects/ui-sdk-designer/input-field';
-import { ButtonModule } from 'projects/ui-sdk-designer/button';
-import { MenuModule } from 'projects/ui-sdk-designer/menu';
-import { RippleModule } from 'ui-sdk/ripple';
-import { ProgressIndicatorModule } from 'projects/ui-sdk-designer/progress-indicator';
-import { DataModelModule } from 'projects/ui-sdk-designer/data-model';
-import { ToastNotificationsModule } from 'projects/ui-sdk-designer/toast-notification';
 import { GlobalErrorHandler } from './global-error-handler';
-import { UIBuilderModule } from 'ui-builder';
 
 
-const UI_SDK_MODULES = [
-  UIBuilderModule.forRoot(),
-  ToastNotificationsModule,
-  InputFieldModule,
-  MenuModule,
-  ButtonModule,
-  RippleModule,
-  ProgressIndicatorModule,
-  DataModelModule
-];
 
 @NgModule({
   declarations: [
@@ -33,8 +13,6 @@ const UI_SDK_MODULES = [
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
-    CdkMenuModule,
-    UI_SDK_MODULES
   ],
   providers: [
      {provide: ErrorHandler, useClass: GlobalErrorHandler}
