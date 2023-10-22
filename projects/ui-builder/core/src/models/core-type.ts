8d1621b98ebd8a5455e4f20385e685f1670fff2e
Farshad Bayat <farshad.bayat@gmail.com>
2023-10-22 20:29:30 +0330

    Add symbol

--- projects/ui-builder/core/src/models/core-type.ts
+++ projects/ui-builder/core/src/models/core-type.ts
@@ -1,7 +1,7 @@
 export type NgStyle = {[key: string]: any};
 export type NgClass = string | string[] | Set<string> | {[key: string]: any};
 // export type sdkType = 'Material' | 'NG-Zero' | 'Bootstrap';
-export type ScalerType = string | number | bigint | boolean | any;
+export type ScalerType = string | number | bigint | boolean | symbol | any;
 export type CollectType<T extends ScalerType = any> = T[];
 
 export interface IDictionary<T=any>{
